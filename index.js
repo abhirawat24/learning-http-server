bfbdc80cc06d054e8cd1df938f1d9bb0495a3889
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2024-04-30 14:48:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.get('/abc', (req, res) => {
+    res.send('Hello page 1');
+});
+
+app.get('/def', (req, res) => {
+    res.send('Hello page 2');
+});
+app.get('/ghi', (req, res) => {
+    res.send('Hello page 3');
+});
+app.get('/jkl', (req, res) => {
+    res.send('Hello page 4');
+});
+
+app.listen(port, () => {
+    console.log(`Your port request ${port}`);
+});
