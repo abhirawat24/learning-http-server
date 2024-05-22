e606ea36472768adaeb267ec32b231ddef017a64
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2024-05-22 23:56:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
-
+//different ports lead to different pages
 app.get('/abc', (req, res) => {
     res.send('Hello page 1');
 });
